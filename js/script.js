8abb7e986c03d55734e642b129e3b56d40b4da1a
havus <havus.it@gmail.com>
2019-03-29 20:20:52 +0700

    fix function

--- js/script.js
+++ js/script.js
@@ -1,11 +1,39 @@
-function allClear(bilangan) {
+var master = [];
+
+var result = parseInt(document.getElementsByName('display')[0].value);
+
+var allClear = function (bilangan) {
     bilangan.value = 0;
 }
 
-function addNumber(input, number) {
-    if (input.value == null || input.value == 0) {
-        input.value = number;
+var addNumber = function (number) {
+    var angka = document.getElementsByName('display')[0];
+    if (angka.value.length == 0 || angka.value == 0) {
+        angka.value = number;
+        console.log('kosong')
     } else {
-        input.value += number;
+        angka.value += number;
+        console.log('tdk kosong');
     }
 }
+
+// var plusNumber = function (input) {
+//     if (parseInt(input.value.length) === 0 || parseInt(input.value) === 0) {
+//         // console.log('kosong');
+//         // result = 0;
+//         console.log('kosong');
+//     } else {
+//         var bawah = document.getElementsByName('result')[0];
+//         bawah.value = bawah.value + parseInt(input.value) + '+';
+//         // input.value = parseInt(input) + '+';
+//         console.log('tidak kosong');
+//         // return input;
+//     }
+// }
+
+// var hasil = "1 + 1";
+
+// console.log(eval(hasil);
+// console.log(display);
+
+// console.log(document.getElementsByName('result')[0].value)
