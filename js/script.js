b53b49602fb30e28292859408400dc4b8453170c
havus <havus.it@gmail.com>
2019-03-30 00:40:42 +0700

    change font

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ var addChar = function (number) {
     } else {
         angka.value += number;
     }
-}
+};
 
 var addDoubleNull = function (number) {
     if (angka.value.length == 0 || angka.value == 0) {
@@ -14,29 +14,22 @@ var addDoubleNull = function (number) {
     } else {
         angka.value += number;
     }
-}
+};
 
 var allClear = function (bilangan) {
     bilangan.value = 0;
-}
+};
 
 var hitung = function () {
-    angka.value = eval(angka.value);
-}
+    // fix no undefined result
+    if (angka.value.length == 0 || angka.value == 0) {
+        angka.value = 0;
+    } else {
+        angka.value = eval(angka.value);
+    }
+};
 
 var delChar = function () {
     var result = angka.value.slice(0, -1);
     angka.value = result;
-}
-
-// var namaFungsi = (data1, data2) =>
-//     console.log(data1 + data2);
-
-// namaFungsi(1, 9);
-
-// var addDoubleNull = (number) =>
-//     if (angka.value.length == 0 || angka.value == 0) {
-//         angka.value = 0;
-//     } else {
-//         angka.value += number;
-//     };
+};
